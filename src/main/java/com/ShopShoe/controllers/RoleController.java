ad3a9a23955405488c01e683a7b08aba5e39155b
NguyenTinh03032000 <NguyenTinh03032000@gmail.com>
2022-03-31 11:15:02 +0700

    add config log

--- src/main/java/com/ShopShoe/controllers/RoleController.java
+++ src/main/java/com/ShopShoe/controllers/RoleController.java
@@ -2,17 +2,11 @@ package com.ShopShoe.controllers;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +18,7 @@ import com.ShopShoe.repository.RoleRepository;
 @RequestMapping("role")
 @PreAuthorize("hasRole('ADMIN')")
 public class RoleController {
+
 	private static Logger logger = Logger.getLogger(RoleController.class);
 	
 	@Autowired
@@ -43,42 +38,42 @@ public class RoleController {
 		return roleRepository.getById(id);
 	}
 	
-	@PostMapping()
-	public String createRole(@RequestBody RoleEntity role) {
-		try {
-			if(roleRepository.existsByName(role.getName())){
-				return "Role already exist";
-			}else {
-				roleRepository.save(role);
-				return "Add role successful";
-			}
-		} catch (Exception e) {
-			return "Error";
-		}
-	}
-	
-	@DeleteMapping("/{id}")
-	public String deleteRole(@PathVariable Integer id){
-		try {
-			RoleEntity role = roleRepository.getById(id);
-			
-			roleRepository.delete(role);
-			return "Delete role successful";			
-		} catch (Exception e) {
-			return "Error";
-		}	
-	}
-	
-	@PutMapping("/{id}")
-	public String updateRole(@PathVariable(value = "id") Integer id,@Valid @RequestBody RoleEntity roleDetails) {
-		try {
-			RoleEntity role = roleRepository.getById(id);
-			role.setName(roleDetails.getName());
-			
-			roleRepository.save(role);
-			return "Update role successfuly";
-		}catch (Exception e) {
-			return "Error";
-		}
-	}
+//	@PostMapping()
+//	public String createRole(@RequestBody RoleEntity role) {
+//		try {
+//			if(roleRepository.existsByName(role.getName())){
+//				return "Role already exist";
+//			}else {
+//				roleRepository.save(role);
+//				return "Add role successful";
+//			}
+//		} catch (Exception e) {
+//			return "Error";
+//		}
+//	}
+//	
+//	@DeleteMapping("/{id}")
+//	public String deleteRole(@PathVariable Integer id){
+//		try {
+//			RoleEntity role = roleRepository.getById(id);
+//			
+//			roleRepository.delete(role);
+//			return "Delete role successful";			
+//		} catch (Exception e) {
+//			return "Error";
+//		}	
+//	}
+//	
+//	@PutMapping("/{id}")
+//	public String updateRole(@PathVariable(value = "id") Integer id,@Valid @RequestBody RoleEntity roleDetails) {
+//		try {
+//			RoleEntity role = roleRepository.getById(id);
+//			role.setName(roleDetails.getName());
+//			
+//			roleRepository.save(role);
+//			return "Update role successfuly";
+//		}catch (Exception e) {
+//			return "Error";
+//		}
+//	}
 }
--- src/main/java/com/ShopShoe/controllers/UserController.java
+++ src/main/java/com/ShopShoe/controllers/UserController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Set;
 
 import javax.validation.Valid;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
